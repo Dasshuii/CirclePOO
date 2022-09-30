2766c1e70533810393ae14a6504060eda17e1a53
Daniel Ramirez <daniel.ramirzc22@gmail.com>
2022-09-29 19:16:18 -0600

    Add project files.

---
+++ POOPractice1/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POOPractice1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle myCircle = new Circle(5);
+            Console.WriteLine(myCircle.ToString());
+
+            Console.Read();
+        }
+    }
+
+    class Circle
+    {
+        // Variables
+        private const double PI = 3.14;
+        private double radius = 0;
+
+        //Constructors
+        public Circle(){}
+        public Circle(double radius) { this.radius = radius; }
+
+        // Getter/Setter
+
+        public double Radius {
+            get { return radius; }
+            set { radius = value; }
+        }
+
+        //Methods
+        public double Area() {
+            return PI * radius * radius;
+        }
+
+        public double Perimeter()
+        {
+            return 2 * PI * radius;
+        }
+
+        //ToString
+        public override string ToString()
+        {
+            return "My radius is: " + radius + '\n' + "My area is: " + Area() + '\n' + "My perimeter is: " + Perimeter(); 
+        }
+
+        //Destructor
+        ~Circle() {
+        }
+    }
+}
